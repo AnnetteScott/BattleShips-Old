346a9c43ea246f090351950fd3859bba40b99e3c
NotNatural21 <notnatural21@gmail.com>
2023-08-31 11:57:23 +1200

    init project

---
+++ BattleShips/src/battleships/BattleShips.java
@@ -0,0 +1,16 @@
+package battleships;
+
+/**
+ *
+ * @author Annet
+ */
+public class BattleShips {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
