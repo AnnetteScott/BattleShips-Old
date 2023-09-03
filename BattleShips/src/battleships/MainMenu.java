0fb7dd391e00f88232b95c3a410e4cc6297bbefa
NotNatural21 <notnatural21@gmail.com>
2023-09-03 17:21:37 +1200

    Moved Start Game button to centre of screen

--- BattleShips/src/battleships/MainMenu.java
+++ BattleShips/src/battleships/MainMenu.java
@@ -9,7 +9,17 @@ import javax.swing.*;
 public class MainMenu extends JPanel {
 
     public MainMenu() {
-        JButton startButton = new JButton("Start");
+        setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridwidth = GridBagConstraints.REMAINDER;
+        constraints.anchor = GridBagConstraints.NORTH;
+
+        add(new JLabel("<html><h1><strong>Battleships</strong></h1></html>"), constraints);
+
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+
+        JButton startButton = new JButton("<html><h1><strong>Start Game</strong></h1></html>");
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -17,7 +27,13 @@ public class MainMenu extends JPanel {
                 cardLayout.show(getParent(), "Game");
             }
         });
-
-        add(startButton);
+        
+        
+        JPanel buttons = new JPanel(new GridBagLayout());
+        buttons.add(startButton, constraints);
+        buttons.add(highScores, constraints);
+        
+        constraints.weighty = 1;
+        add(buttons, constraints);
     }
 }
