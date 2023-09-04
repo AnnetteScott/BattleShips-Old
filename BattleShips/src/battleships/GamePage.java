385a091f7d1ab2084a086eb830e7d124db321a57
NotNatural21 <notnatural21@gmail.com>
2023-09-04 12:12:27 +1200

    Added basic game grid

--- BattleShips/src/battleships/GamePage.java
+++ BattleShips/src/battleships/GamePage.java
@@ -2,15 +2,24 @@ package battleships;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
 /**
  * @author gmt3870
  */
 public class GamePage extends JPanel{
     
+    final static int BOARD_SIZE = 10;
+    
     public GamePage(){
-        JButton startButton = new JButton("Menu");
-        startButton.addActionListener(new ActionListener() {
+        setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        
+        JPanel header = new JPanel();
+        header.setSize(1200, 40);
+        header.setPreferredSize(new Dimension(1200, 40));
+        
+        JButton menuButton = new JButton("Menu");
+        menuButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 CardLayout cardLayout = (CardLayout) getParent().getLayout();
@@ -18,6 +27,32 @@ public class GamePage extends JPanel{
             }
         });
 
-        add(startButton);
+        header.add(menuButton);
+        add(header);
+        
+        JPanel gameBoard = new JPanel(new GridLayout(BOARD_SIZE + 1, BOARD_SIZE + 1));
+        gameBoard.setSize(300, 300);
+        gameBoard.add(new JLabel());
+        for(int i = 0; i < BOARD_SIZE; i++){
+            char letter = (char)('A' + i);
+            JLabel display = new JLabel(String.valueOf(letter));
+            display.setBorder(new MatteBorder(1, 1, 1, 1, Color.BLACK));
+            gameBoard.add(display);
+        }
+        
+        for(int i = 0; i < BOARD_SIZE; i++){
+            JLabel num = new JLabel(String.valueOf(i + 1));
+            num.setBorder(new MatteBorder(1, 1, 1, 1, Color.BLACK));
+            gameBoard.add(num);
+            
+            for(int j = 0; j < BOARD_SIZE; j++){
+                JLabel display = new JLabel();
+                display.setBorder(new MatteBorder(1, 1, 1, 1, Color.BLACK));
+                gameBoard.add(display);
+            }
+            
+        }
+        
+        add(gameBoard, BorderLayout.CENTER);
     }
 }
